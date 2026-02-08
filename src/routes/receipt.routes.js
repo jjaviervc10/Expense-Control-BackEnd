58942eaca0d11209f29ffc3b65dc039f0fca46db
jjaviervc10 <v.cjavier.velazquez@gmail.com>
2026-02-08 01:22:32 -0600

    fix: corregir import/export en receipt.routes.js para compatibilidad CommonJS

--- src/routes/receipt.routes.js
+++ src/routes/receipt.routes.js
@@ -1,9 +1,10 @@
-import { Router } from 'express';
-import ReceiptController from '../controllers/receipt.controller.js';
+
+const { Router } = require('express');
+const ReceiptController = require('../controllers/receipt.controller.js');
 
 const router = Router();
 
 // Endpoint para procesar ticket
 router.post('/process', ReceiptController.processReceipt);
 
-export default router;
+module.exports = router;
