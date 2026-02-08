f6f3ca0ba799563b38d7b3143b321e26fce620d3
jjaviervc10 <v.cjavier.velazquez@gmail.com>
2026-02-08 13:28:48 -0600

    fix: usa process.env.PORT como único puerto para compatibilidad Railway

--- src/app.js
+++ src/app.js
@@ -58,9 +58,9 @@ import receiptRoutes from "./routes/receipt.routes.js";
 app.use("/api/receipt", receiptRoutes);
 
 // Puerto
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-        console.log("Servidor backend escuchando en puerto", PORT);
+    console.log("Servidor backend escuchando en puerto", PORT);
 });
 
 
